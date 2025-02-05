0a4fb9ca195c7db69c32b7408bd9613cd5adb002
Allan11231123 <122254867+Allan11231123@users.noreply.github.com>
2025-02-05 12:01:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['./config/initialpose.yaml','./config/tf_static.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
