cb428e67f946068a3f97b3eb95dc60f681df3a68
Allan11231123 <122254867+Allan11231123@users.noreply.github.com>
2025-02-05 17:03:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'velocity_receiver'
 
@@ -10,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['./config/initialpose.yaml','./config/tf_static.yaml']),
+        (os.path.join('share',package_name,'config'),glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
